48922a4f5721dabed87bfe020c0e57403184dc79
Karen Chiquillo <nifttykae@gmail.com>
2026-09-19 17:21:38 -0500

    feat(service): implementar ReservaService con reglas de solapamiento y horario

---
+++ src/main/java/com/universidad/reservaslabs/exception/ReservaConflictException.java
@@ -0,0 +1,7 @@
+package com.universidad.reservaslabs.exception;
+
+public class ReservaConflictException extends RuntimeException {
+    public ReservaConflictException(String mensaje) {
+        super(mensaje);
+    }
+}
